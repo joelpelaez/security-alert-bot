c00a33aea12d13288fb15ea2a51c00ec37e7bb8f
Joel Pelaez Jorge <joel.pelaez.jorge@gmail.com>
2025-08-22 20:15:16 -0500

    fix: parse multiple date formats in FreeBSD data source

--- ds/freebsd_ds.py
+++ ds/freebsd_ds.py
@@ -7,14 +7,40 @@
 # LICENSE file in the root directory of this source tree) and the GPLv3 (found
 # in the COPYING file in the root directory of this source tree).
 # You may select, at your option, one of the above-listed licenses.
+from datetime import date, datetime
 
 import feedparser
+import dateparser
 
 from domain.datasource import DataSource
 from model.entry import Entry
 
 
+def parse_date(data: str | date | datetime) -> datetime | None:
+    if isinstance(data, date):
+        return datetime(data.year, data.month, data.day)
+
+    if isinstance(data, datetime):
+        return data
+
+    if isinstance(data, str):
+        try:
+            # First try parse as ISO date format
+            return datetime.fromisoformat(data)
+        except ValueError:
+            pass
+
+        # Try using dateparser, if it fails returns None
+        return dateparser.parse(data)
+
+
 class FreeBSDDataSource(DataSource):
+    """FreeBSD base and kernel security advertisement and errata notices data source.
+
+    This data sources retrieves information about FreeBSD base system security
+    advertisements and errata notices from the official RSS source.
+    """
+
     def get_entries(self) -> list[Entry]:
         feed = feedparser.parse("https://www.freebsd.org/security/rss.xml")
         entries = []
@@ -24,7 +50,7 @@ class FreeBSDDataSource(DataSource):
                     title=entry.title,
                     content=entry.summary,
                     link=entry.link,
-                    pub_date=entry.published,
+                    pub_date=parse_date(entry.published),
                 )
             )
         return entries
