e31c8abaf7a34e4c646ba4f5bcc8043efee2ad08
Joel Pelaez Jorge <joel.pelaez.jorge@gmail.com>
2026-03-10 15:36:35 -0500

    chore: add missing function and docstrings for Manager class

--- domain/manager.py
+++ domain/manager.py
@@ -10,11 +10,39 @@
 
 
 class Manager:
+    """
+    Define a service manager for all interactions between the bots and the data sources.
+    It must handle synchronization and race conditions between bot activation and data
+    retrival.
+    """
+
     def on_ready(self):
+        """
+        Called when the bot is ready for send and receive messages.
+        """
         pass
 
     def on_terminate(self):
+        """
+        Called when the bot has terminated.
+        """
         pass
 
     async def start_async(self):
+        """
+        Async main loop for the manager, must be called on an async context.
+        """
+        pass
+
+    async def lock(self):
+        """
+        Allow to lock the manager waiting for the related bot to become ready.
+
+        The bot must call `on_ready()` for release the lock and start the manager's
+        main loop.
+
+        See also:
+            `on_ready`
+            `start_async`
+        """
         pass
